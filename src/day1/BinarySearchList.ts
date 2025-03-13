d2743eeddec3fecb116ed2c465735e79828dfce6
Chris Lyon <chris@lyon.la>
2025-03-13 00:29:31 -0700

    binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,20 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
 
+    do {
+        const mid = Math.floor((lo + (hi - lo) / 2));
+        const item = haystack[mid];
+
+        if (item === needle) {
+            return true;
+        } else if (item < needle) {
+            lo = mid + 1;
+        } else {
+            // item >= needle
+            hi = mid;
+        }
+    } while (hi > lo);
+
+    return false;
 }
