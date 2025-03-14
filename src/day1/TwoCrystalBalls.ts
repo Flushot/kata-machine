6ca0f9fe9a8c32936e2c964253af89fef76fb795
Chris Lyon <chris@lyon.la>
2025-03-13 18:54:00 -0700

    2 crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,21 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const chunkSize = Math.floor(Math.sqrt(breaks.length));
+    const chunkCount = Math.floor(breaks.length / chunkSize);
 
+    for (let chunkIdx = 0; chunkIdx < chunkCount; ++chunkIdx) {
+        const chunkPtr = chunkIdx * chunkSize;
+        const chunk = breaks.slice(chunkPtr, chunkPtr + chunkSize);
+        if (!chunk[chunk.length - 1]) {
+            continue;
+        }
+
+        // Backtrack into chunk to find first match
+        for (let slotIdx = 0; slotIdx < chunk.length; ++slotIdx) {
+            if (chunk[slotIdx]) {
+                return chunkPtr + slotIdx;
+            }
+        }
+    }
+
+    return -1;
 }
