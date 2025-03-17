9d1c91e3319844e47312297b4ff1c717d3d8685b
Chris Lyon <chris@lyon.la>
2025-03-16 17:38:18 -0700

    breadth first search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,27 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+import Queue from "./Queue";
 
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const queue = new Queue<BinaryNode<number>>();
+    queue.enqueue(head);
+
+    while (queue.length > 0) {
+        const node = queue.deque();
+        if (!node) {
+            continue;
+        }
+
+        if (node.value === needle) {
+            return true;
+        }
+
+        if (node.left) {
+            queue.enqueue(node.left);
+        }
+
+        if (node.right) {
+            queue.enqueue(node.right);
+        }
+    }
+
+    return false;
 }
